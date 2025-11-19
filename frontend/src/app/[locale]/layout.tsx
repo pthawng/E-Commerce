0761f87a91c89b7155c6ef3e6bb520d99e5e4b8c
pthawng <lephuocthang207@gmail.com>
2025-11-19 17:34:47 +0700

    refactor: FE types

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -3,13 +3,10 @@ import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 import { setRequestLocale, getMessages } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
+import { LocaleLayoutProps } from '@/types/next-intl';
 
-type Props = {
-  children: React.ReactNode;
-  params: Promise<{ locale: string }>;
-};
 
-export default async function LocaleLayout({ children, params }: Props) {
+export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
   const { locale } = await params;
 
   if (!hasLocale(routing.locales, locale)) {
---
+++ frontend/src/types/next-intl.d.ts
@@ -0,0 +1,5 @@
+export type LocaleLayoutProps = {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+};
+
