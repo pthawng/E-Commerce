e0ecaec13d219e0cbb81f9afee1aef0d17eba4e3
pthawng <lephuocthang207@gmail.com>
2025-11-28 17:48:49 +0700

    feat(BE): Joi (Nestjs Read .env)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+ConfigModule.forRoot({
+  isGlobal: true,
+  validationSchema: Joi.object({
+    PORT: Joi.number().default(3000),
+    DATABASE_URL: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
+  }),
+});
+
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
